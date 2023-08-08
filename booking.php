56ec06d04fdec9e5889a672ff96961d34f6c9a27
jlangella1 <jacklangella2001@gmail.com>
2023-08-08 18:53:56 +0100

    add on

--- booking.php
+++ booking.php
@@ -7,32 +7,40 @@ $dbname = "booking_database"; // Replace with your database name
 // Create a new MySQLi instance
 $mysqli = new mysqli($servername, $username, $password, $dbname);
 $dbflag = false;
+
 // Check connection
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 } else {
-    echo "Connected successfully";
     $dbflag = true;
-    //echo $dbflag;  //test line to check var, shows 1 for true
 }
-if (isset($_POST['booking_day'], $_POST["booking_time"], $_POST['booking_name'],  $_POST['booking_pnumber'])) {
-     
 
-    //now do the form code here
-    //getting content from html form -> save to php var -> pass to mysql var 
-    //var_dump($_POST["booking_day"], $_POST["booking_time"],$_POST["booking_name"], $_POST["booking_pnumber"]); //html fields
+if (isset($_POST['booking_day'], $_POST["booking_time"], $_POST['booking_name'],  $_POST['booking_pnumber'])) {
+    // Getting content from HTML form and saving to PHP variables
     $days = htmlspecialchars($_POST["booking_day"]);
     $hours = htmlspecialchars($_POST["booking_time"]);
     $booking_name = htmlspecialchars($_POST["booking_name"]);
     $booking_pnumber = htmlspecialchars($_POST["booking_pnumber"]);
     
-    $query ="INSERT INTO booking (booking_day, booking_time, booking_name, booking_pnumber) VALUES ( '  ".$days." ','  ".$hours." ',' ".$booking_name." ',' ".$booking_pnumber."   ' )";	
-    mysqli_query($mysqli, $query); 
+    // Inserting data into the database
+    $query = "INSERT INTO booking (booking_day, booking_time, booking_name, booking_pnumber) VALUES ('$days', '$hours', '$booking_name', '$booking_pnumber')";
+    if (mysqli_query($mysqli, $query)) {
+        // Success! Booking inserted into the database
 
+        // Store the confirmation status in a session variable
+        session_start();
+        $_SESSION['booking_confirmation'] = true;
+
+        // Redirect back to index.html
+        header('Location: index.html');
+        exit;
+    } else {
+        // Handle the case when the database operation fails
+        // You can add your own error handling code here
+        echo "Error: " . $query . "<br>" . mysqli_error($mysqli);
+    }
 }
-// Perform your database operations here
 
 // Close the connection
 $mysqli->close();
-// echo "<p>working</p>"; //testing line to check file is being picked up
 ?>
